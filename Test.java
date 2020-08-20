13bff5c721e62fc0360633506fdc427f0dab5583
sbwww <45259695+sbwww@users.noreply.github.com>
2020-08-20 18:58:25 +0800

    add test

--- Test.java
+++ Test.java
@@ -1,8 +1,12 @@
-package com.OrderRecipe.client;
+package com.OrderRecipe;
+
+import javax.swing.JOptionPane;
+
+import com.OrderRecipe.client.LoginFrame;;
 
 public class Test {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Copyright 2020 Bowen Shen, NUIST\nDo not change this dialog!", "版权声明", JOptionPane.INFORMATION_MESSAGE);
+    	JOptionPane.showMessageDialog(null, "Copyright 2020 Bowen Shen, NUIST\nDo not change this dialog!", "版权声明", JOptionPane.INFORMATION_MESSAGE);
         new LoginFrame("顾客选桌").showMe();
     }
 }
